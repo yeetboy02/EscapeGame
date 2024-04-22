c888e8699963d852ebd3a586f69a98c01fa6e7d9
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-04-22 12:19:14 +0200

    add Program.cs game loop

---
+++ game/Program.cs
@@ -0,0 +1,32 @@
+﻿using libs;
+
+class Program
+{    
+    static void Main(string[] args)
+    {
+        //Setup
+        Console.CursorVisible = false;
+        var engine = GameEngine.Instance;
+        var inputHandler = InputHandler.Instance;
+        
+        engine.Setup();
+
+        // Main game loop
+        while (true)
+        {
+            // Handle keyboard input
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            inputHandler.Handle(keyInfo);
+
+            engine.Update();
+
+            engine.Render();
+
+            // CHECK WIN CONDITION
+            // if (engine.checkWinCond()) {
+            //     break;
+            // }
+        }
+    }
+
+}
