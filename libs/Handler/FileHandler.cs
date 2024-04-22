741688b80c56d3e76ec82fe93dc7797767b56ea0
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-04-22 12:55:16 +0200

    added fileHandler

---
+++ libs/Handler/FileHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace libs;
+
+using Newtonsoft.Json;
+
+public static class FileHandler
+{
+    private static string filePath;
+    private readonly static string envVar = "GAME_SETUP_PATH";
+
+    static FileHandler()
+    {
+        Initialize();
+    }
+
+    private static void Initialize()
+    {
+        if(Environment.GetEnvironmentVariable(envVar) != null){
+            filePath = Environment.GetEnvironmentVariable(envVar);
+        };
+    }
+
+    public static dynamic ReadJson()
+    {
+        if (string.IsNullOrEmpty(filePath))
+        {
+            throw new InvalidOperationException("JSON file path not provided in environment variable");
+        }
+
+        try
+        {
+            string jsonContent = File.ReadAllText(filePath);
+            dynamic jsonData = JsonConvert.DeserializeObject(jsonContent);
+            return jsonData;
+        }
+        catch (FileNotFoundException)
+        {
+            throw new FileNotFoundException($"JSON file not found at path: {filePath}");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Error reading JSON file: {ex.Message}");
+        }
+    }
+}
